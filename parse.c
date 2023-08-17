e4d4ae6ea752f48b324941957b76dfb5a1671899
huolongguo1O <121071167+huolongguo1O@users.noreply.github.com>
2023-08-17 16:57:29 +0800

    modified:   parse.c 	modified:   main.c 	modified:   makefile 	modified:   toplevel.h 	modified:   types/types.c 	modified:   utils/utils.c 	modified:   utils/utils.h

--- parse.c
+++ parse.c
@@ -2,6 +2,7 @@
 context ctx;
 char * parse(char * code,char end_char){
 	char * ret = malloc(sizeof(char)*MAX_STR_LEN);
+	memset(ret,0,sizeof(char)*MAX_STR_LEN);
 	int pret=0;
 	int max_len_plus_times=2;
 	while(code[ctx.ip]!=end_char){
@@ -9,6 +10,7 @@ char * parse(char * code,char end_char){
 			ctx.ip++;
 			char * tmp = parse(code,')');
 			free(tmp);
+			goto end;
 		}
 		if(code[ctx.ip]=='{'){
 			ctx.ip++;
@@ -23,14 +25,16 @@ char * parse(char * code,char end_char){
 					MAX_STR_LEN*
 					(max_len_plus_times++)
 				);
+				memset(_ret, 0, MAX_STR_LEN*(max_len_plus_times-1));
 				strcpy(_ret,ret);
 				strcpy(_ret+pret,tmp);
 				pret+=strlen(tmp);
 				free(ret);
-				free(tmp);
+				//free(tmp);
 				ret = _ret;
 			}
 			free(tmp);
+			goto end;
 		}
 		if(code[ctx.ip]=='['){
 			if(pret+1+10 <= MAX_STR_LEN*(max_len_plus_times-1)){
@@ -43,6 +47,7 @@ char * parse(char * code,char end_char){
 					MAX_STR_LEN*
 					(max_len_plus_times++)
 				);
+				memset(_ret, 0, MAX_STR_LEN*(max_len_plus_times-1));
 				strcpy(_ret,ret);
 				//ctx.ip++;
 				strcat(_ret+pret,"Bad syntax");
@@ -51,9 +56,11 @@ char * parse(char * code,char end_char){
 				ret = _ret;
 
 			}
+			goto end;
 		}
 		if(code[ctx.ip]=='$'){
 			char * var_name=malloc(sizeof(char)*MAX_STR_LEN);
+			memset(var_name,0,sizeof(char)*MAX_STR_LEN);
 			int pvar_name=0;
 			int max_len_plus_times_tmp=2;
 			while(code[++ctx.ip]!='$'){
@@ -65,12 +72,14 @@ char * parse(char * code,char end_char){
 						MAX_STR_LEN*
 						(max_len_plus_times_tmp++)
 					);
+					memset(_tmp,0,sizeof(char)*MAX_STR_LEN*(max_len_plus_times_tmp-1));
 					strcpy(_tmp,var_name);
-					free(var_name);
+					
 					var_name=_tmp;
 					var_name[pvar_name++]=code[ctx.ip];
 				}
 			}
+			
 			char * tmp=tostring(get_var(var_name));
 			if(pret+1+strlen(tmp) <= MAX_STR_LEN*(max_len_plus_times-1)){
 				strcat(ret+pret,tmp);
@@ -82,20 +91,29 @@ char * parse(char * code,char end_char){
 					MAX_STR_LEN*
 					(max_len_plus_times++)
 				);
+				memset(_ret, 0, MAX_STR_LEN*(max_len_plus_times-1));
 				strcpy(_ret,ret);
 				strcpy(_ret+pret,tmp);
 				pret+=strlen(tmp);
 				free(ret);
-				free(tmp);
+				
 				ret = _ret;
 			}
+			_dprintf("var_name: %s\n",var_name);
+			//free(tmp);
+			free(var_name);
+			goto end;
 		}
 		if(code[ctx.ip]=='\\'){
 			char * tmp=malloc(sizeof(char)*MAX_STR_LEN);
+			memset(tmp,0,sizeof(char)*MAX_STR_LEN);
 			int count=0;
-			while(code[++ctx.ip]!='{'|| 
-				code[ctx.ip]!=' '||
-				code[ctx.ip]!='\0') {
+			//_dprintf("Fn searching \n");
+			while(code[++ctx.ip]!='{'&&
+				code[ctx.ip]!=' '&&
+				code[ctx.ip]!='\0'&&
+				code[ctx.ip]!=end_char) {
+					//_dprintf("ctx.ip=%d, char=%c\n",ctx.ip,code[ctx.ip]);
 					if(count+1>MAX_STR_LEN){
 						if(pret+1+10 <= MAX_STR_LEN*(max_len_plus_times-1)){
 							strcpy(ret+pret,"Bad syntax");
@@ -107,6 +125,7 @@ char * parse(char * code,char end_char){
 								MAX_STR_LEN*
 								(max_len_plus_times++)
 							);
+							memset(_ret, 0, MAX_STR_LEN*(max_len_plus_times-1));
 							strcpy(_ret,ret);
 							//ctx.ip++;
 							strcat(_ret+pret,"Bad syntax");
@@ -119,15 +138,62 @@ char * parse(char * code,char end_char){
 					}
 					tmp[count++]=code[ctx.ip];
 			}
-
+			//_dprintf("Fn name:%s\n",tmp);
 			while(code[ctx.ip]=='{'){
 				++ctx.ip;
-				parse(code,'{');
-				++ctx.ip;
+				char * _tmp = parse(code,'}');
+				//_dprintf("Parse result: %s\n",_tmp);
+				//++ctx.ip;
+			}
+			//--ctx.ip;
+			//--ctx.ip;
+			//++ctx.ip;
+			goto end;
+		}
+		end:
+		if(code[ctx.ip]!='\\'&&
+			code[ctx.ip]!='{'&&
+			code[ctx.ip]!='$'&&
+			code[ctx.ip]!='('&&
+			code[ctx.ip]!='['){
+			if(pret+2<=MAX_STR_LEN*(max_len_plus_times-1))
+				ret[pret++]=code[ctx.ip];
+			else{
+				char * _ret = malloc(
+					sizeof(char)*
+					MAX_STR_LEN*
+					(max_len_plus_times++));
+				memset(_ret, 0, MAX_STR_LEN*(max_len_plus_times-1));
+				strcpy(_ret,ret);
+				//ctx.ip++;
+				ret[pret++]=code[ctx.ip];
+				free(ret);
+				ret = _ret;
 			}
 		}
-		
+		//else
 		ctx.ip++;
 	}
+	ctx.ip++;
+	_dprintf("_Parse_result: %s\n", ret);
 	return ret;
+}
+
+char * parse(char * code, char end){
+	char * ret = malloc(sizeof(char)*MAX_STR_LEN);
+	memset(ret, 0, MAX_STR_LEN);
+	int pret = 0;
+	int count = 0;
+	int plus_time=1;
+	while(code[ctx.ip]!=end){
+		switch(code[ctx.ip]){
+			case '{':
+				char * tmp=parse(code,'}');
+				ret=str_appened(ret,tmp,&pret,&plus_times);
+				break;
+			case '(':
+				char * tmp=parse(code,')');
+				break;
+		}
+	}
 }
